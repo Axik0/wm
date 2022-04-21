d9a1d9fadb706ffdf4ec9f7e8af9a11968c04ce4
Axik0 <xla.2@yandex.ru>
2022-04-21 23:50:41 +0300

    wm overlay gain

--- interface.py
+++ interface.py
@@ -26,32 +26,55 @@ class MainPage(GridLayout):
     result_image = ObjectProperty(None)
     wm_coordinates = ObjectProperty(None)
 
-    def img_click(self, image_size, img_container_size, offset):
-        """we assume that our image fits square container by width or height"""
-        global_coord = Window.mouse_pos
-        img_proportion = image_size[0]/image_size[1]
+    def test(self, arg):
+        print(arg)
+
+    def img_coord_calc(self, global_coord, image_size, img_container_size, offset):
+        """When we click on our image area, we get some raw_coord related to the whole app window.
+        This method transforms those to the local coordinates of some point of current image.
+        We assume that our window has default size and each image fits its square container automatically.
+        Usage of round function results in <0.5px inaccuracy for our filler calc which is also assumed negligible"""
+        # first, note that our image is superseded by one button (starting from the very bottom)
         int_cont_coord = [global_coord[_] - offset[_] for _ in range(2)]
+        # as we can't get width and height to compare, let's use full_resolution to get proportion
+        img_proportion = image_size[0]/image_size[1]
+        # we assume that our image fits square container by width or height
         if img_proportion >= 1:
+            # horizontal fit case, we have two equal top and bottom filler stripes surrounding our image
             filler_ds = (0, round(abs(img_container_size[0] * (1 - 1/img_proportion)))/2)
             inf_crop = filler_ds[1]
             sup_crop = img_container_size[1] - filler_ds[1]
         else:
+            # vertical fit case, left and right filler stripes
             filler_ds = (round(abs(img_container_size[0] * (1 - img_proportion)))/2, 0)
             inf_crop = filler_ds[0]
             sup_crop = img_container_size[0] - filler_ds[0]
+        # we don't need anything except our image, that's why we should apply a condition first
         if inf_crop < int_cont_coord[0] < sup_crop or inf_crop < int_cont_coord[1] < sup_crop:
             self.wm_coordinates = [int_cont_coord[_] - filler_ds[_] for _ in range(2)]
-            print(self.wm_coordinates)
+            # print(self.wm_coordinates)
+
+    def img_click(self, image_size, img_container_size, offset):
+        global_coords = Window.mouse_pos
+        self.img_coord_calc(global_coords, image_size, img_container_size, offset)
+
+        self.ids.text_wm.text = 'WWWW'
+        self.ids.text_wm.pos = global_coords
+        print(global_coords)
+
+        self.ids.text_wm.source = 'test.jpg'
+        self.ids.text_wm.opacity = 1
+        self.ids.text_wm.pos = global_coords
 
     def main_img_load(self):
-        self.main_img.source = 'test.jpg'
-
+        self.show_load()
+        self.ids.main_img.source = 'test.jpg'
+        print(self.loaded_image)
 
     def text_wm(self, text, opacity):
         print(text)
         print(opacity)
         self.ids.main_img.source = 'test.jpg'
-        print(self.ids)
 
     def show_load(self):
         content = LoadDialog(load=self.load, cancel=self.dismiss_popup)
@@ -94,3 +117,4 @@ class ImageBtn(ButtonBehavior, Image):
 
 if __name__ == '__main__':
     Watermark().run()
+
