324a44e97e642b731b28ebc95616eef2978bb528
Axik0 <xla.2@yandex.ru>
2022-05-05 01:56:23 +0300

    GUI has been connected to main image processor image watermark function has been partially implemented, but have to map coordinates and image size properly text watermark functionality hasn't been tested yet

--- interface.py
+++ interface.py
@@ -6,7 +6,7 @@ import kivy
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.floatlayout import FloatLayout
-from kivy.properties import ObjectProperty, BooleanProperty
+from kivy.properties import ObjectProperty, BooleanProperty, StringProperty, NumericProperty
 from kivy.uix.popup import Popup
 from kivy.core.window import Window
 from kivy.uix.behaviors import ButtonBehavior
@@ -14,6 +14,7 @@ from kivy.uix.image import Image
 from kivy.core.image import Image as CoreImage
 
 from PIL import Image as Img, ImageFont, ImageDraw
+
 
 class Watermark(App):
     Window.minimum_height = 600
@@ -23,14 +24,16 @@ class Watermark(App):
     def build(self):
         return MainPage()
 
+
 class MainPage(GridLayout):
     # looks like we can leave properties without def init and self.* at the beginning but they will work the same way
-    temp_image_texture = ObjectProperty(None)
-    result_image = ObjectProperty(None)
+    loaded_image = ObjectProperty(None)
+    main_image = ObjectProperty(None)
+    wm_image = ObjectProperty(None)
+    wm_text = StringProperty(None)
+    wm_text_opacity = NumericProperty(None)
     wm_coordinates = ObjectProperty(None)
 
-    def test(self, arg):
-        print(arg)
 
     def img_coord_calc(self, global_coord, inscr_image_size, img_container_size, offset):
         """When we click on our image area, we get some raw_coord related to the whole app window.
@@ -52,8 +55,8 @@ class MainPage(GridLayout):
             sup_crop = img_container_size[0] - filler_ds[0]
         # we don't need anything except our image, that's why we should apply a condition first
         if inf_crop < int_cont_coord[0] < sup_crop or inf_crop < int_cont_coord[1] < sup_crop:
-            self.wm_coordinates = [round(int_cont_coord[_] - filler_ds[_]) for _ in range(2)]
-            # print(self.wm_coordinates)
+            self.wm_coordinates = [[round(int_cont_coord[_] - filler_ds[_]) for _ in range(2)], inscr_image_size]
+            print(self.wm_coordinates)
 
     def img_click(self, inscr_image_size, img_container_size, offset):
         global_coords = Window.mouse_pos
@@ -63,7 +66,8 @@ class MainPage(GridLayout):
         self.ids.img_wm.pos = global_coords
 
     def text_wm(self, text, opacity):
-        self.ids.text_wm.text = text
+        self.wm_text = self.ids.text_wm.text = text
+        self.wm_text_opacity = opacity
         self.ids.text_wm.color[3] = opacity/255
 
     def show_load(self, action_type):
@@ -76,26 +80,33 @@ class MainPage(GridLayout):
         self._popup.open()
 
     def show_save(self):
-        content = SaveDialog(save=self.save, cancel=self.dismiss_popup)
-        self._popup = Popup(title="Save image", content=content, size_hint=(0.9, 0.9))
-        self._popup.open()
-        # self.ids.main_img.source = 'result.jpg'
-        # self.ids.text_wm.text = ''
-        # self.ids.img_wm.opacity = 0
+        """temporarily used for image processing"""
+        from main import process
+        process(self.main_image, self.wm_coordinates, self.wm_image, self.wm_text, self.wm_text_opacity)
+
+    # def show_save(self):
+    #     content = SaveDialog(save=self.save, cancel=self.dismiss_popup)
+    #     self._popup = Popup(title="Save image", content=content, size_hint=(0.9, 0.9))
+    #     self._popup.open()
+    #     # self.ids.main_img.source = 'result.jpg'
+    #     # self.ids.text_wm.text = ''
+    #     # self.ids.img_wm.opacity = 0
 
     def dismiss_popup(self, action_type=None):
         self._popup.dismiss()
         if action_type is not None:
             if action_type:
-                self.ids.img_wm.texture = self.temp_image_texture
+                self.wm_image = self.loaded_image
+                self.ids.img_wm.texture = self.temp_image_tx
                 self.ids.img_wm.opacity = 0.5
             else:
-                self.ids.main_img.texture = self.temp_image_texture
+                self.main_image = self.loaded_image
+                self.ids.main_img.texture = self.temp_image_tx
 
     def load(self, action_type, path, filename):
         with open(os.path.join(path, filename[0]), 'rb') as stream:
-            loaded_image = io.BytesIO(stream.read())
-        self.temp_image_texture = CoreImage(loaded_image, ext='png').texture
+            self.loaded_image = io.BytesIO(stream.read())
+        self.temp_image_tx = CoreImage(self.loaded_image, ext='png').texture
         self.dismiss_popup(action_type)
 
     def save(self, path, filename):
@@ -116,7 +127,7 @@ class SaveDialog(FloatLayout):
 
 
 class ImageBtn(ButtonBehavior, Image):
-    """adds button on_press capability to an Image"""
+    """adds button on_press capability to an Image, note that ButtonBehavior has to be the very first one"""
     pass
 
 
--- main.py
+++ main.py
@@ -10,23 +10,40 @@ base_image = Image.open("okacho.jpg").convert("RGBA")
 # blend images
 
 
-ch_opacity = 255
-ch_text = "Okacho"
+# ch_opacity = 255
+# ch_text = "Okacho"
 ch_font = ImageFont.truetype("arial.ttf", 40)
 
-# measure dimensions of a box with our text with a font given
-ch_text_size = ch_font.getsize(ch_text)
-# print(ch_text_size)
-# first, create blank image, don't matter what's the fill but zero opacity
-txt = Image.new('RGBA', size=ch_text_size, color=(255, 255, 255, 0))
-# then we draw a single text line on top of that with nonzero opacity, left-top anchor alignment
-d = ImageDraw.Draw(txt)
-d.text((0, 0), text=ch_text, font=ch_font, anchor="lt", fill=(255, 255, 255, ch_opacity))
-ch_position = (base_image.size[0] - ch_text_size[0], 7 + base_image.size[1] - ch_text_size[1])
-# Both our images must have mode RGBA to use this particular method
-base_image.alpha_composite(txt, dest=ch_position)
-base_image.show()
 
-result = base_image.convert('RGB')
-result.save("name.jpg")
+# # measure dimensions of a box with our text with a font given
+# ch_text_size = ch_font.getsize(ch_text)
+# # print(ch_text_size)
+# # first, create blank image, don't matter what's the fill but zero opacity
+# txt = Image.new('RGBA', size=ch_text_size, color=(255, 255, 255, 0))
+# # then we draw a single text line on top of that with nonzero opacity, left-top anchor alignment
+# d = ImageDraw.Draw(txt)
+# d.text((0, 0), text=ch_text, font=ch_font, anchor="lt", fill=(255, 255, 255, ch_opacity))
+# ch_position = (base_image.size[0] - ch_text_size[0], 7 + base_image.size[1] - ch_text_size[1])
+# # Both our images must have mode RGBA to use this particular method
+# base_image.alpha_composite(txt, dest=ch_position)
+# base_image.show()
 
+# result = base_image.convert('RGB')
+# result.save("name.jpg")
+
+def process(main_image, coordinates, wm_image, wm_text=None, wm_text_cfg=None):
+    base = Image.open(main_image).convert("RGBA")
+    if wm_text is None:
+        wm = Image.open(wm_image).convert("RGBA")
+    else:
+        # measure dimensions of a box with our text with a font given
+        text_size = ch_font.getsize(wm_text)
+        # print(ch_text_size)
+        # first, create blank image, don't matter what's the fill but zero opacity
+        wm = Image.new('RGBA', size=text_size, color=(255, 255, 255, 0))
+        # then we draw a single text line on top of that with nonzero opacity, left-top anchor alignment
+        d = ImageDraw.Draw(wm)
+        d.text((0, 0), text=wm_text, font=ch_font, anchor="lt", fill=(255, 255, 255, wm_text_cfg))
+    position = (int(coordinates[0][0]/coordinates[1][0]*base.size[0]), base.size[1] - int(coordinates[0][1]/coordinates[1][1]*base.size[1]))
+    base.alpha_composite(wm, dest=position)
+    base.show()
